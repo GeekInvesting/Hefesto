8bd6c3e42706d3bab37b7c413167d621a5e00777
Heder Milani <hedermilani@hotmail.com>
2023-04-05 15:06:14 -0300

    HEF-10 #time 2h #comment Create rounter from updating to Exchange

--- src/main/java/com/api/hefesto/controller/ExchangeController.java
+++ src/main/java/com/api/hefesto/controller/ExchangeController.java
@@ -9,9 +9,11 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,35 +44,35 @@ public class ExchangeController {
     private ExchangeModel exchangeModel;
 
     @PostMapping
-    public ResponseEntity<Object> createExchange(@Valid @RequestBody ExchangeDto exchangeDto){
+    public ResponseEntity<Object> createExchange(@Valid @RequestBody ExchangeDto exchangeDto) {
         LOG.info("Create exchange: " + exchangeDto.toString());
 
-        if(exchangeDto.getExchangeName() == null){
+        if (exchangeDto.getExchangeName() == null) {
             throw new NotAcceptableException("Exchange Name is Required!");
         }
-        if(exchangeService.existsByName(exchangeDto.getExchangeName())){
+        if (exchangeService.existsByName(exchangeDto.getExchangeName())) {
             throw new DataConflictException("Exchange Name already exists! " + exchangeDto.getExchangeName());
         }
         exchangeModel.setExchangeName(exchangeDto.getExchangeName().trim().toUpperCase());
 
-        if(exchangeDto.getExchangeCode() == null) {
+        if (exchangeDto.getExchangeCode() == null) {
             throw new NotAcceptableException("Exchange Code is Required!");
         }
-        if(exchangeService.existsByCode(exchangeDto.getExchangeCode())){
+        if (exchangeService.existsByCode(exchangeDto.getExchangeCode())) {
             throw new DataConflictException("Exchange Code already exists! " + exchangeDto.getExchangeCode());
         }
         exchangeModel.setExchangeCode(exchangeDto.getExchangeCode().trim().toUpperCase());
 
-        if(exchangeDto.getExchangeCountry() == null){
+        if (exchangeDto.getExchangeCountry() == null) {
             throw new NotAcceptableException("Exchange Country is Required!");
         }
         Optional<CountryModel> countryModel = countryService.findByName(exchangeDto.getExchangeCountry());
-        if(countryModel.isEmpty()){
+        if (countryModel.isEmpty()) {
             throw new ResourceNotFoundException("Exchange Country not found! " + exchangeDto.getExchangeCountry());
         }
         exchangeModel.setExchangeCountry(countryModel.get());
 
-        //TODO: Adicionar validação de moeda
+        // TODO: Adicionar validação de moeda
         exchangeModel.setExchangeCurrency(exchangeDto.getExchangeCurrency().trim().toUpperCase());
 
         exchangeModel.setExchangeEnabled(true);
@@ -79,32 +81,119 @@ public class ExchangeController {
         ExchangeModel exchangeCreated = exchangeService.saveExchange(exchangeModel);
 
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-        .path("{/id}")
-        .buildAndExpand(exchangeCreated.getId())
-        .toUri();
+                .path("{/id}")
+                .buildAndExpand(exchangeCreated.getId())
+                .toUri();
 
         return ResponseEntity.created(uri).body(exchangeCreated);
     }
 
     @GetMapping("all")
-    public ResponseEntity<Object> getAllExchange(){
+    public ResponseEntity<Object> getAllExchange() {
         LOG.info("Get all exchange");
         return ResponseEntity.status(HttpStatus.OK).body(exchangeService.findAllExchange());
     }
 
     @GetMapping
-    public ResponseEntity<Object> getAllExchangeNotDeleted(){
+    public ResponseEntity<Object> getAllExchangeNotDeleted() {
         LOG.info("Get all exchange not deleted");
         return ResponseEntity.status(HttpStatus.OK).body(exchangeService.findAllExchangeNotDeleted());
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<Object> getById(@PathVariable UUID id){
+    public ResponseEntity<Object> getExchangeById(@PathVariable UUID id) {
         LOG.info("Get exchange by id: " + id);
         Optional<ExchangeModel> exchangeModel = exchangeService.findById(id);
-        if(exchangeModel.isEmpty()){
+        if (exchangeModel.isEmpty()) {
             throw new ResourceNotFoundException("Exchange not found! " + id);
         }
         return ResponseEntity.status(HttpStatus.OK).body(exchangeModel.get());
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Object> updateExchange(
+            @Valid @RequestBody ExchangeDto exchangeDto,
+            @PathVariable UUID id) {
+        LOG.info("Exchange Updating: " + exchangeDto.toString());
+
+        Optional<ExchangeModel> exchangeModel = exchangeService.findById(id);
+        if (exchangeModel.isEmpty()) {
+            throw new ResourceNotFoundException("Exchange not found! " + id);
+        }
+
+        if (exchangeDto.getExchangeName() != null){
+            exchangeModel.get().setExchangeName(exchangeDto.getExchangeName().trim().toUpperCase());
+        }
+        if(exchangeDto.getExchangeCode() != null){
+            exchangeModel.get().setExchangeCode(exchangeDto.getExchangeCode().trim().toUpperCase());
+        }
+        if(exchangeDto.getExchangeCountry() != null){
+            Optional<CountryModel> countryModel = countryService.findByName(exchangeDto.getExchangeCountry());
+            if (countryModel.isEmpty()) {
+                throw new ResourceNotFoundException("Exchange Country not found! " + exchangeDto.getExchangeCountry());
+            }
+            exchangeModel.get().setExchangeCountry(countryModel.get());
+        }
+        if(exchangeDto.getExchangeCurrency() != null){
+            exchangeModel.get().setExchangeCurrency(exchangeDto.getExchangeCurrency().trim().toUpperCase());
+        }
+
+        exchangeModel.get().setExchangeEnabled(true);
+        exchangeModel.get().setExchangeDeleted(false);
+
+        ExchangeModel exchangeUpdate = exchangeService.saveExchange(exchangeModel.get());
+
+        return ResponseEntity.status(HttpStatus.OK).body(exchangeUpdate);
+    }
+
+    @PutMapping("enabled/{id}")
+    public ResponseEntity<Object> enableExchange(@PathVariable UUID id){
+        LOG.info("Enable exchange: " + id);
+
+        Optional<ExchangeModel> exchangeModel = exchangeService.findById(id);
+        if (exchangeModel.isEmpty()) {
+            throw new ResourceNotFoundException("Exchange not found! " + id);
+        }
+
+        exchangeModel.get().setExchangeEnabled(true);
+        exchangeModel.get().setExchangeDeleted(false);
+
+        ExchangeModel exchangeUpdate = exchangeService.saveExchange(exchangeModel.get());
+
+        return ResponseEntity.status(HttpStatus.OK).body(exchangeUpdate);
+    }
+
+    @PutMapping("disabled/{id}")
+    public ResponseEntity<Object> disableExchange(@PathVariable UUID id){
+        LOG.info("Disable exchange: " + id);
+
+        Optional<ExchangeModel> exchangeModel = exchangeService.findById(id);
+        if (exchangeModel.isEmpty()) {
+            throw new ResourceNotFoundException("Exchange not found! " + id);
+        }
+
+        exchangeModel.get().setExchangeEnabled(false);
+        exchangeModel.get().setExchangeDeleted(false);
+
+        ExchangeModel exchangeUpdate = exchangeService.saveExchange(exchangeModel.get());
+
+        return ResponseEntity.status(HttpStatus.OK).body(exchangeUpdate);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Object> deleteExchange(@PathVariable UUID id){
+        LOG.info("Delete exchange: " + id);
+
+        Optional<ExchangeModel> exchangeModel = exchangeService.findById(id);
+        if (exchangeModel.isEmpty()) {
+            throw new ResourceNotFoundException("Exchange not found! " + id);
+        }
+
+        exchangeModel.get().setExchangeEnabled(false);
+        exchangeModel.get().setExchangeDeleted(true);
+
+        ExchangeModel exchangeUpdate = exchangeService.saveExchange(exchangeModel.get());
+
+        return ResponseEntity.status(HttpStatus.OK).body(exchangeUpdate);
     }
 }
