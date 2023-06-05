43150321976d18fad9b7b5751c2ba2fa5d5df184
Heder Milani <hedermilani@hotmail.com>
2023-06-05 15:25:21 -0300

    HEF-39 #tima 6h #comment Implement routing for update the ticket

--- src/main/java/com/api/hefesto/controller/TicketController.java
+++ src/main/java/com/api/hefesto/controller/TicketController.java
@@ -2,6 +2,7 @@ package com.api.hefesto.controller;
 
 import java.net.URI;
 import java.util.Optional;
+import java.util.UUID;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.LogManager;
@@ -9,7 +10,10 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,7 +41,7 @@ public class TicketController {
     @Autowired
     private TicketService ticketService;
 
-    @Autowired 
+    @Autowired
     private ExchangeService exchangeService;
 
     @Autowired
@@ -53,51 +57,53 @@ public class TicketController {
     public ResponseEntity<Object> createTicket(@RequestBody TicketDto ticketDto) {
         LOG.info("Create Ticket: " + ticketDto.toString());
 
-        if(StringUtils.isBlank(ticketDto.getTicketCode())){
+        if (StringUtils.isBlank(ticketDto.getTicketCode())) {
             throw new NotAcceptableException("Ticket code is required");
         }
 
         TicketModel ticketModel = new TicketModel(
                 ticketDto.getTicketCode().toUpperCase().trim());
-        
-        if(StringUtils.isNotBlank(ticketDto.getTicketAlpha())){
+
+        if (StringUtils.isNotBlank(ticketDto.getTicketAlpha())) {
             ticketModel.setTicketAlpha(ticketDto.getTicketAlpha().toUpperCase().trim());
         }
 
-        if(StringUtils.isNotBlank(ticketDto.getTicketCvmCode())){
+        if (StringUtils.isNotBlank(ticketDto.getTicketCvmCode())) {
             ticketModel.setTicketCvmCode(ticketDto.getTicketCvmCode().toUpperCase().trim());
         }
 
-        if(StringUtils.isNotBlank(ticketDto.getTicketExchangeCode())) {
-            Optional<ExchangeModel> exchangeSearch = exchangeService.getExchangeByCode(ticketDto.getTicketExchangeCode());
-            if(!exchangeSearch.isPresent()){
+        if (StringUtils.isNotBlank(ticketDto.getTicketExchangeCode())) {
+            Optional<ExchangeModel> exchangeSearch = exchangeService
+                    .getExchangeByCode(ticketDto.getTicketExchangeCode());
+            if (!exchangeSearch.isPresent()) {
                 throw new NotAcceptableException("Exchange not found");
             }
 
             ticketModel.setTicketExchange(exchangeSearch.get());
         }
 
-        if(StringUtils.isNotBlank(ticketDto.getTicketCompanyName())){
+        if (StringUtils.isNotBlank(ticketDto.getTicketCompanyName())) {
             Optional<CompanyModel> companySearch = companyService.getCompanyByName(ticketDto.getTicketCompanyName());
-            if(!companySearch.isPresent()){
+            if (!companySearch.isPresent()) {
                 throw new NotAcceptableException("Company not found");
             }
 
             ticketModel.setTicketCompany(companySearch.get());
         }
 
-        if(StringUtils.isNotBlank(ticketDto.getTicketSubsectorName())){
-            Optional<SubsectorModel> subsectorSearch = subsectorService.getSubsectorByName(ticketDto.getTicketSubsectorName());
-            if(!subsectorSearch.isPresent()){
+        if (StringUtils.isNotBlank(ticketDto.getTicketSubsectorName())) {
+            Optional<SubsectorModel> subsectorSearch = subsectorService
+                    .getSubsectorByName(ticketDto.getTicketSubsectorName());
+            if (!subsectorSearch.isPresent()) {
                 throw new NotAcceptableException("Subsector not found");
             }
 
             ticketModel.setTicketSubsector(subsectorSearch.get());
         }
 
-        if(StringUtils.isNotBlank(ticketDto.getTicketTypeCode())){
+        if (StringUtils.isNotBlank(ticketDto.getTicketTypeCode())) {
             Optional<TypeModel> typeSearch = typeService.getTypeByCode(ticketDto.getTicketTypeCode());
-            if(!typeSearch.isPresent()){
+            if (!typeSearch.isPresent()) {
                 throw new NotAcceptableException("Type not found");
             }
 
@@ -110,15 +116,74 @@ public class TicketController {
         TicketModel ticketCreate = ticketService.saveTicket(ticketModel);
 
         URI uri = ServletUriComponentsBuilder
-        .fromCurrentRequest()
-        .path("/{id}")
-        .buildAndExpand(ticketCreate.getId())
-        .toUri();
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(ticketCreate.getId())
+                .toUri();
 
-        //TODO: Implement msg ticketToZeus
+        // TODO: Implement msg ticketToZeus
 
         return ResponseEntity.created(uri).body(ticketCreate);
     }
 
+    @GetMapping("all")
+    public ResponseEntity<Object> getAllTickets() {
+        LOG.info("Get All Tickets");
 
+        return ResponseEntity.ok(ticketService.getAllTickets());
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Object> updateTicket(@PathVariable UUID id, @RequestBody TicketDto ticketDto) {
+        LOG.info("Update Ticket to: " + ticketDto.toString());
+
+        Optional<TicketModel> ticketSearch = ticketService.getTicketById(id);
+
+        if (!ticketSearch.isPresent()) {
+            throw new NotAcceptableException("Ticket not found");
+        }
+
+        ticketSearch.get().setTicketAlpha(
+                ticketDto.getTicketAlpha() == null
+                        ? ticketSearch.get().getTicketAlpha()
+                        : ticketDto.getTicketAlpha().toUpperCase().trim());
+
+        ticketSearch.get().setTicketCvmCode(
+                ticketDto.getTicketCvmCode() == null
+                        ? ticketSearch.get().getTicketCvmCode()
+                        : ticketDto.getTicketCvmCode().toUpperCase().trim());
+
+        Optional<CompanyModel> companySearch = companyService.getCompanyByName(ticketDto.getTicketCompanyName());
+        ticketSearch.get().setTicketCompany(
+                companySearch.isPresent()
+                        ? companySearch.get()
+                        : ticketSearch.get().getTicketCompany());
+
+        Optional<ExchangeModel> exchangeSearch = exchangeService.getExchangeByCode(ticketDto.getTicketExchangeCode());
+        ticketSearch.get().setTicketExchange(
+                exchangeSearch.isPresent()
+                        ? exchangeSearch.get()
+                        : ticketSearch.get().getTicketExchange());
+
+        Optional<TypeModel> typeSearch = typeService.getTypeByCode(ticketDto.getTicketTypeCode());
+        ticketSearch.get().setTicketType(
+                typeSearch.isPresent()
+                        ? typeSearch.get()
+                        : ticketSearch.get().getTicketType());
+
+        Optional<SubsectorModel> subsectorSearch = subsectorService.getSubsectorByName(ticketDto.getTicketSubsectorName());
+        ticketSearch.get().setTicketSubsector(
+                subsectorSearch.isPresent()
+                        ? subsectorSearch.get()
+                        : ticketSearch.get().getTicketSubsector());
+
+        ticketSearch.get().setTicketEnabled(true);
+        ticketSearch.get().setTicketDeleted(false);
+
+        TicketModel ticketUpdate = ticketService.saveTicket(ticketSearch.get());
+
+        //TODO: Implement msg ticket to 
+
+        return ResponseEntity.ok(ticketUpdate);
+    }
 }
