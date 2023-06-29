1e5affdcabe12225bbd06d888662bd2861cf7894
Heder Milani <hedermilani@hotmail.com>
2023-06-29 15:07:29 -0300

    Implement alter msg error exception

--- src/main/java/com/api/hefesto/controller/exception/HefestoControllerAdvice.java
+++ src/main/java/com/api/hefesto/controller/exception/HefestoControllerAdvice.java
@@ -15,37 +15,37 @@ public class HefestoControllerAdvice {
     
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException e) {
-        String errorMessage = "Illegal error Exception: " + e.getMessage();
+        String errorMessage = "{\"message\": \"Illegal error Exception: " + e.getMessage() + "\"}";
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException e) {
-        String errorMessage = "Not Found Exception: " + e.getMessage();
+        String errorMessage = "{\"message\": \"Not Found Exception: " + e.getMessage() + "\"}";
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
     }
 
     @ExceptionHandler(DataConflictException.class)
     public ResponseEntity<Object> handleDataConflictException(DataConflictException e) {
-        String errorMessage = "Data Conflict Exception: " + e.getMessage();
+        String errorMessage = "{\"message\": \"Data Conflict Exception: " + e.getMessage() + "\"}";
         return ResponseEntity.status(HttpStatus.CONFLICT).body(errorMessage);
     }
 
     @ExceptionHandler(NotAcceptableException.class)
     public ResponseEntity<Object> handleNotAcceptableException(NotAcceptableException e) {
-        String errorMessage = "Invalid Data Exception: " + e.getMessage();
+        String errorMessage = "{\"message\": \"Invalid Data Exception: " + e.getMessage() + "\"}";
         return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(errorMessage);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleException(Exception e) {
-        String errorMessage = "Error Exception: " + e.getMessage();
+        String errorMessage = "{\"message\": \"Error Exception: " + e.getMessage() + "\"}";
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
     }
 
     @ExceptionHandler(UnauthorizedException.class)
     public ResponseEntity<Object> handleUnauthorizedException(UnauthorizedException e){
-        String errorMessage = "Token Not is Valid!";
+        String errorMessage = "{\"message\": \"Token Not is Valid!\"}";
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorMessage);
     }
 }
