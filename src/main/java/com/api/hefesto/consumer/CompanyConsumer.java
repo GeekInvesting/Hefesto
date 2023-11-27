94a235cccbe5965d4e1c8b4b0aef86f621bc3958
Heder Milani <hedermilani@hotmail.com>
2023-11-27 15:44:16 -0300

    Implement consumer rabbitmq queue CompanyToHefesto

---
+++ src/main/java/com/api/hefesto/consumer/CompanyConsumer.java
@@ -0,0 +1,33 @@
+package com.api.hefesto.consumer;
+
+import com.api.hefesto.model.CompanyModel;
+import com.api.hefesto.service.CompanyService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CompanyConsumer {
+    private static final Logger LOG = LogManager.getLogger(CompanyConsumer.class);
+
+    @Autowired
+    private CompanyService companyService;
+
+    private CompanyModel company = new CompanyModel();
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @RabbitListener(queues = "${rabbitmq.queue.company.hefesto}")
+    public void CompanyListen(@Payload String companyMq) throws JsonProcessingException {
+        LOG.info("Company Save of Listener: " + companyMq);
+
+        company = mapper.readValue(companyMq, CompanyModel.class);
+
+        LOG.debug("Company Save of Listener: " + company.toString());
+    }
+}
---
+++ src/main/java/com/api/hefesto/dto/CompanyDto.java
@@ -0,0 +1,4 @@
+package com.api.hefesto.dto;
+
+public class CompanyDto {
+}
