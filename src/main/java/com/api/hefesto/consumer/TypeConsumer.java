4c168d62be470d75606fb09756fea6bd173d968a
Heder Milani <hedermilani@hotmail.com>
2023-12-05 15:39:09 -0300

    Implement mapper object type model

--- src/main/java/com/api/hefesto/consumer/TypeConsumer.java
+++ src/main/java/com/api/hefesto/consumer/TypeConsumer.java
@@ -1,5 +1,8 @@
 package com.api.hefesto.consumer;
 
+import com.api.hefesto.model.TypeModel;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -10,8 +13,12 @@ import org.springframework.stereotype.Component;
 public class TypeConsumer {
     private static final Logger LOG = LogManager.getLogger(TypeConsumer.class);
 
+    private final ObjectMapper mapper = new ObjectMapper();
+
     @RabbitListener(queues = "${rabbitmq.queue.type.hefesto}")
-    public void TypeListen(@Payload String typeMq) {
+    public void TypeListen(@Payload String typeMq) throws JsonProcessingException {
         LOG.info("Type Save of Listener: " + typeMq);
+        TypeModel type = mapper.readValue(typeMq, TypeModel.class);
+        LOG.debug("Type Save of Listener: " + type.toString());
     }
 }
